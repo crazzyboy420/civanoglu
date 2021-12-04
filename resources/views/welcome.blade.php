acf7c8ddf78459e284d4cc6e7a01583ef31fca48
crazzyboy420 <raselbabu34.bd@gmail.com>
2021-12-04 22:33:04 +0600

    new update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,4 +13,44 @@
             <a href="#" class="text-xl rounded-2xl py-3 px-8 inline-block border-2 border-gray-700">Start the review</a>
         </div>
     </div>
+    <div class="container text-center">
+        <h2 class="text-4xl mb-10">Why should you choose a Property <br> with the help of our sercives?</h2>
+        <div class="w-full flex mb-10">
+            <div class="w-1/3 px-20">
+                <h3 class="text-xl font-bold mb-2">Huge selection <br> of real estate</h3>
+                <p class="text-gray-500">Choose from the best propertise from the most current developers</p>
+            </div>
+            <div class="w-1/3 px-20">
+                <h3 class="text-xl font-bold mb-2">Full information <br> about objects and buildings</h3>
+                <p class="text-gray-500">Access to main benefits characteristise and feature directly from the objects page</p>
+            </div>
+            <div class="w-1/3 px-20">
+                <h3 class="text-xl font-bold mb-2">The ability to get an online<br> display of the object</h3>
+                <p class="text-gray-500">Book an online display of any of the objects you are intersted in </p>
+            </div>
+        </div>
+        <div class="w-full flex">
+            <div class="w-1/3 px-20">
+                <h3 class="text-xl font-bold mb-2">Buarantee</h3>
+                <p class="text-gray-500">We take the transaction support on ourselves</p>
+            </div>
+            <div class="w-1/3 px-20">
+                <h3 class="text-xl font-bold mb-2">Assistance in selection<br> of real estate</h3>
+                <p class="text-gray-500">Our experts will help you choose exactly what you have been dreaming about for a long time</p>
+            </div>
+            <div class="w-1/3 px-20">
+                <h3 class="text-xl font-bold mb-2">Related information</h3>
+                <p class="text-gray-500">We maintain up-to-date information</p>
+            </div>
+        </div>
+        <div class="w-full">
+            <h2 class="text-4xl mb-10 mt-10">More information about us</h2>
+            <div style="background-image:url(/img/video-background.jpg)" class="w-full bg-cover bg-center flex justify-center items-center rounded">
+                <div class="py-20 text-center">
+                    <a href="#"><img src="/img/play-btn.png" alt=""></a>
+                    <p class="text-2xl text-white -ml-4">Watch the video</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-guest-layout>
